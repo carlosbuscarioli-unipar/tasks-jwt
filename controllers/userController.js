8ecca92d9e045e36061a973d70fc39c757c23a19
carlosbuscarioli-unipar <carlos.buscarioli@edu.unipar.br>
2025-09-16 22:48:54 -0300

    Updated package.json

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {generateToken} = require('../middlewares/authMiddleware')
 const login = async (req, res) => {
   try {
